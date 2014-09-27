5263e393a15d2f398099dfc8cd6fdfdc71f19660
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2014-09-27 12:08:45 -0500

    Website folder structure
    - Created the folder structure for the website using express, jade and
    stylus.
    - Added MongoDB module for connection to the database.

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/consulta', function (req, res){
+	var resu = [];
+	var collection = req.db.collection('users');
+      //Trae todos los documentos de la colección: "users"
+      collection.find().toArray(function(err, results) {
+        //Muestra los resultados en el navegador.
+        res.send(results);
+      });
+});
+
+module.exports = router;
