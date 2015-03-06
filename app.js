e2cffa87cbc35820896110f8b54754f0cf095bcc
Giovanni Mendoza <mendozagioo@gmail.com>
2015-03-05 20:46:50 -0600

    Initial commit

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -39,7 +38,6 @@ app.use(function(req,res,next){
 });
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,35 +1,14 @@
-var Bubble = function() {
-  var Buttons = [{
-        text:"Completar mi registro",
-        CallBack:function() {
-         // alert("clickeaste al boton");
-        }
-      }];
+$(document).ready(function() {
+  function set_height() {
+    var width = $(window).width(),
+        height = $(window).height();
 
-  var BubbleObject = {id:1,title:"First Message", body:"Body Message",Buttons : Buttons}
-  var Move = function () {
-      console.log('move');
-      $("#n1").animate({marginLeft: "0.09in"},0,MoveBack)
-  }
-  var MoveBack = function(){
-      console.log('moveBack');
-      $("#n1").animate({marginLeft: "0.02in"},500)
+    $('#home').css('height', height);
   }
 
-  return {
-    Run : function () {
-      var currentInterval = setInterval(Move,1000);
-      $("#bubble-able > div").mouseenter(function() {
-        console.log('mouse over');
-        clearInterval(currentInterval);
-        $(this).animate({width:"400px",height:"400px"},250,function(){alert("");})
-      });
+  set_height();
 
-      $("#bubble-able > div").mouseout(function() {
-        $(this).animate({width:"40px",height:"40px"},250,function(){alert("");});
-      });
-    }
-  }
-}();
-
-Bubble.Run();
+  $(window).resize(function() {
+    set_height();
+  });
+});
