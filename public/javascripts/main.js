b7ad5745aa1fb851545fb207e0a9d3acc78588b6
Giovanni Mendoza <mendozagioo@gmail.com>
2015-03-10 03:06:53 -0600

    Release 0.0.1

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  var shown = false;
+
   function set_height() {
     var width = $(window).width(),
         height = $(window).height();
@@ -10,5 +12,29 @@ $(document).ready(function() {
 
   $(window).resize(function() {
     set_height();
+  });
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > $('#home').height()) {
+      $('#menu').css('position', 'fixed');
+    } else {
+      $('#menu').css('position', 'relative');
+    }
+  });
+
+  $('.show-menu').click(function (e) {
+    e.preventDefault();
+
+    if(!shown) {
+      $('#menu').css('height', 'auto');
+      $('#menu .link').css('display', 'block');
+      $('#menu .socials').css('display', 'block');
+      shown = true;
+    } else {
+      $('#menu').css('height', '80px');
+      $('#menu .link').css('display', 'none');
+      $('#menu .socials').css('display', 'none');
+      shown = false;
+    }
   });
 });
